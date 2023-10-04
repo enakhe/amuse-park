4f6454a315fccd5aa7e752c832da7983280c8e35
Izuagbe Samuel Enakhe <izuagbesamuel73@gmail.com>
2023-10-04 11:50:46 +0100

    Add the three card item for rides btw zero to fifty

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,3 @@
 $(document).ready(function() {
-    $("#entry").submit(function(e) {
-        e.preventDefault();
-
-        const age = parseInt($("#age").val());
-
-        if (age < 21) {
-            $(".message").fadeIn();
-            $(".drink").fadeOut();
-        } else {
-            $(".message").fadeOut();
-            $(".drink").fadeIn();
-        }
-    })
+    
 })
