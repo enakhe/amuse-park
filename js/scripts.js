dde0687edf85f05261d8f4fb98b52b208fd77751
Izuagbe Samuel Enakhe <izuagbesamuel73@gmail.com>
2023-10-04 12:05:53 +0100

    Add the functionality to show rides between zero to fifty height

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-    
+    $("#amuse-form").submit(function(e) {
+        e.preventDefault();
+
+        const name = $("#name").val();
+        const height = parseInt($("#height").val());
+
+        // If statement
+        if (height <= 50) {
+
+            $(".userName").text(name);
+
+            $(".entryForm").fadeOut(500);
+            $(".zero-fifty").fadeIn(500);
+
+            $("#zero-fifty-back").click(function() {
+                $(".zero-fifty").fadeOut(500);
+                $(".entryForm").fadeIn(500);
+            })
+        }
+    })
 })
